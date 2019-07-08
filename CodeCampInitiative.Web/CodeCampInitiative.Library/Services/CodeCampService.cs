3084095ae3be82c23875ae5911c799d38ee312d2
Sachith Jayasinghe <SachithJ@99x.lk>
2019-07-08 19:13:33 +0530

    Added code camp service with basic operations

---
+++ CodeCampInitiative.Web/CodeCampInitiative.Library/Services/CodeCampService.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using CodeCampInitiative.Data.Entities;
+using CodeCampInitiative.Data.Interfaces;
+using CodeCampInitiative.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CodeCampInitiative.Library.Services
+{
+    public class CodeCampService
+    {
+        private readonly IEntityRepository<CodeCamp> _repository;
+        private readonly IMapper _mapper;
+
+        public CodeCampService(IEntityRepository<CodeCamp> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CodeCampModel>> GetCodeCamps()
+        {
+            return _mapper.Map<IEnumerable<CodeCampModel>>(await _repository.GetAllAsync());
+        }
+
+        public async Task<CodeCampModel> GetCodeCamp(int id)
+        {
+            if (id == 0)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            return _mapper.Map<CodeCampModel>(await _repository.GetByIdAsync(id));
+        }
+
+        public async Task<CodeCampModel> UpdateCodeCamp(int id, CodeCampModel codeCampModel)
+        {
+            if (id == 0 || codeCampModel == null)
+            {
+                throw new ArgumentNullException(nameof(codeCampModel));
+            }
+
+            if (!await this.CodeCampExists(id))
+            {
+                throw new InvalidOperationException();
+            }
+
+            _repository.Update(_mapper.Map<CodeCamp>(codeCampModel));
+            await _repository.SaveAsync();
+
+            return await GetCodeCamp(id);
+        }
+
+
+        public async Task<CodeCampModel> PostCodeCamp(CodeCampModel codeCampModel)
+        {
+            if (codeCampModel == null)
+            {
+                throw new ArgumentNullException(nameof(codeCampModel));
+            }
+            var codeCamp = _mapper.Map<CodeCamp>(codeCampModel);
+            _repository.Insert(codeCamp);
+            await _repository.SaveAsync();
+
+            return await GetCodeCamp(codeCamp.Id);
+        }
+
+        public async Task DeleteCodeCamp(int id)
+        {
+            if (id == 0)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            if (await this.CodeCampExists(id))
+            {
+                _repository.Delete(id);
+                await _repository.SaveAsync();
+            }
+
+        }
+
+
+        private async Task<bool> CodeCampExists(int id)
+        {
+            return await _repository.GetByIdAsync(id) != null;
+        }
+    }
+}
---
+++ CodeCampInitiative.Web/CodeCampInitiative.Web/Controllers/CodeCampsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CodeCampInitiative.Data.Data;
+using CodeCampInitiative.Data.Entities;
+
+namespace CodeCampInitiative.Web.Controllers
+{
+    public class CodeCampsController : ApiController
+    {
+        private CodeCampDbContext db = new CodeCampDbContext();
+
+        // GET: api/CodeCamps
+        public IQueryable<CodeCamp> GetCodeCamps()
+        {
+            return db.CodeCamps;
+        }
+
+        // GET: api/CodeCamps/5
+        [ResponseType(typeof(CodeCamp))]
+        public IHttpActionResult GetCodeCamp(int id)
+        {
+            CodeCamp codeCamp = db.CodeCamps.Find(id);
+            if (codeCamp == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(codeCamp);
+        }
+
+        // PUT: api/CodeCamps/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCodeCamp(int id, CodeCamp codeCamp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != codeCamp.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(codeCamp).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CodeCampExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CodeCamps
+        [ResponseType(typeof(CodeCamp))]
+        public IHttpActionResult PostCodeCamp(CodeCamp codeCamp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CodeCamps.Add(codeCamp);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = codeCamp.Id }, codeCamp);
+        }
+
+        // DELETE: api/CodeCamps/5
+        [ResponseType(typeof(CodeCamp))]
+        public IHttpActionResult DeleteCodeCamp(int id)
+        {
+            CodeCamp codeCamp = db.CodeCamps.Find(id);
+            if (codeCamp == null)
+            {
+                return NotFound();
+            }
+
+            db.CodeCamps.Remove(codeCamp);
+            db.SaveChanges();
+
+            return Ok(codeCamp);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CodeCampExists(int id)
+        {
+            return db.CodeCamps.Count(e => e.Id == id) > 0;
+        }
+    }
+}
