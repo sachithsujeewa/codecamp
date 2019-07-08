5a586bb9525658c965361315ad03982e505fb8f0
Sachith Jayasinghe <SachithJ@99x.lk>
2019-07-08 19:05:38 +0530

    Added Entity Models

---
+++ CodeCampInitiative.Web/CodeCampInitiative.Data/Models/CodeCampModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeCampInitiative.Data.Models
+{
+    public class CodeCampModel
+    {
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        //public LocationModel Location { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; } = 1;
+        //public ICollection<SessionModel> Sessions { get; set; }
+    }
+}
---
+++ CodeCampInitiative.Web/CodeCampInitiative.Data/Models/SessionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeCampInitiative.Data.Models
+{
+    public class SessionModel
+    {
+        //public CodeCampModel CodeCamp { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public int Level { get; set; }
+        //public SpeakerModel Speaker { get; set; }
+    }
+}
