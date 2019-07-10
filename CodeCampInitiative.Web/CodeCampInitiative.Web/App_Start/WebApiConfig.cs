18935da817a0ec862ce2739b96b5ea70175832f1
Sachith Jayasinghe <SachithJ@99x.lk>
2019-07-10 16:38:06 +0530

    Added a camel case contract resolver

--- CodeCampInitiative.Web/CodeCampInitiative.Web/App_Start/WebApiConfig.cs
+++ CodeCampInitiative.Web/CodeCampInitiative.Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Serialization;
 using System.Web.Http;
 
 namespace CodeCampInitiative.Web
@@ -13,6 +11,8 @@ namespace CodeCampInitiative.Web
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            // Add camel case contract resolver
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
